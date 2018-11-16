82b9307ff4575d7e99403a2367e6b217cbf20df7
Ajay Ramachandran <dev@ajay.app>
2018-11-16 13:08:48 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {dialogflow} = require('actions-on-google');
+const functions = require('firebase-functions');
+
+const app = dialogflow({debug: true});
+
+app.intent('Default Welcome Intent', (conv) => {
+  // Do things
+});
+
+exports.yourAction = functions.https.onRequest(app);
