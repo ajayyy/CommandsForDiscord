0bb1a60d39005b0807a30d383945de68cb702d5e
Ajay Ramachandran <dev@ajay.app>
2018-11-17 10:54:43 -0500

    responds to web hooks and uses https

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-'use strict';
+// Initialize WebHooks module.
+var WebHooks = require('node-webhooks')
 
-const {dialogflow} = require('actions-on-google');
-const functions = require('firebase-functions');
+//setup https
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/domain.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/domain.crt', 'utf8');
 
-const app = dialogflow({debug: true});
+var credentials = {key: privateKey, cert: certificate};
 
-app.intent('Default Welcome Intent', (conv) => {
-  // Do things
+//setup express
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+    var body = req.body;
+
+    console.log(body);
+
+    res.json({
+        final_response: 'ok got it!'
+    });
 });
 
-exports.yourAction = functions.https.onRequest(app);
+//setup server
+var httpsServer = https.createServer(credentials, app);
+
+var server = httpsServer.listen(port, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Server listening at port %s', port)
+});
